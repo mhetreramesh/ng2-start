f4fcfbb51a2e293107f800949df7cf3b8b801301
Ramesh Mhetre <mhetreramesh@gmail.com>
2016-10-17 13:54:34 +0530

    Added http example
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- dist/app.component.js
+++ dist/app.component.js
@@ -9,17 +9,33 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var http_1 = require('@angular/http');
+// Import RxJs required methods
+require('rxjs/add/operator/map');
+require('rxjs/add/operator/catch');
 var AppComponent = (function () {
-    function AppComponent() {
-        this.enquiries = [{ id: 1, name: 'Ramesh', username: "rameshm" }];
+    function AppComponent(http) {
+        this.http = http;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.getEnquiries();
+    };
+    AppComponent.prototype.getEnquiries = function () {
+        var _this = this;
+        var headers = new http_1.Headers({ 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6IjRmMWcyM2ExMmFhIn0.eyJpc3MiOiJodHRwOlwvXC9hcGkudmVnZnJ1LmNvbVwvIiwianRpIjoiNGYxZzIzYTEyYWEiLCJ1aWQiOjEsInVzZXJfaWQiOjEyNzUxLCJpYXQiOjE0NzY2MTg2ODcsImV4cCI6MTQ3NzgyODI4N30.3MCdvVH2RAOEyP3d2l8v5UdN1QZtZaLcSg24i1nfKYM' });
+        var options = new http_1.RequestOptions({ headers: headers });
+        var enquiryData = this.http.get('http://demo.vegfru.com/api/v1/enquiries?limit=10&search=buyer&searchFields=customer_type:=&orderBy=date_time&sortedBy=DESC', options)
+            .map(function (res) { return res.json(); })
+            .subscribe(function (data) { return _this.enquiriesList = data.data; }, function (err) { return console.log(err); });
+        this.enquiries = [{ id: 1, name: 'Ramesh', email: "rameshm" }];
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
             templateUrl: './app/app.component.html',
             styleUrls: ['./app/app.component.css']
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], AppComponent);
     return AppComponent;
 }());
